ae45b8cae4c3b564737ed6f5bcec1017be87e60c
elaaadani <73748351+elaaadani@users.noreply.github.com>
2020-11-15 00:44:16 -0300

    Control de funciones temperatura API

--- src/app.js
+++ src/app.js
@@ -44,9 +44,17 @@ function showTemperature(response)
 {
     console.log(response.data.main.temp);
     let temperatureElement =document.querySelector("#temperature");
-    temperatureElement.innerHTML= response.data.main.temp;
+    let cityElement = document.querySelector("h1#city");
+    let descriptionEelement = document.querySelector("h3#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    cityElement.innerHTML=response.data.main.name;
+    temperatureElement.innerHTML= Math.round(response.data.main.temp);
+    descriptionEelement.innerHTML= response.data.weather[0].description;
+    humidityElement.innerHTML=response.data.main.humidity;
+    windElement.innerHTML=Math.round(response.data.wind.speed);
 }
 let apiKey = "921a29e043e83d24341a625517d5a318";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Santiago&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
